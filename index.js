c221b11ec9b9fa818e91b3438e83306cfd6e41e1
sumon paul <sumpaul100@gmail.com>
2023-10-16 19:02:32 +0600

    updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
     // Get the database and collection on which to run the operation
     const coffeCollection = client.db("coffeeDB").collection("coffee");
     const usersCollections = client.db("coffeeDB").collection("users")
